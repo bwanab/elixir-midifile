ce34186c6ab3b4a4269ddd62f1ebba2977925892
Bill Allen <bill@k2bea.org>
2025-05-11 11:14:05 -0400

    simplify event construction for chords

--- lib/midifile/event.ex
+++ lib/midifile/event.ex
@@ -38,27 +38,27 @@ defmodule Midifile.Event do
   end
 
 
-  @spec new(event_type(), Chord.t(), integer()) :: [t()]
-  def new(:chord, chord, tpqn) when chord.notes == nil do
-    [first | others] = Chord.chord_to_notes(chord.chord)
-    first_event = first_chord_note(first, chord.duration, tpqn)
-    other_events = Enum.map(others, &(other_chord_notes(&1)))
-    raw = [first_event | other_events]
-    # raw is now a list of :on :off pairs, we want to gather all the :on
-    # events at the start and all the :off events at the end.
-    Enum.map(raw, &(Enum.at(&1, 0))) ++ Enum.map(raw, &(Enum.at(&1, 1)))
-  end
+  # @spec new(event_type(), Chord.t(), integer()) :: [t()]
+  # def new(:chord, chord, tpqn) do # when chord.notes == nil do
+  #   [first | others] = Chord.chord_to_notes(chord.chord)
+  #   first_event = first_chord_note(first, chord.duration, tpqn)
+  #   other_events = Enum.map(others, &(other_chord_notes(&1)))
+  #   raw = [first_event | other_events]
+  #   # raw is now a list of :on :off pairs, we want to gather all the :on
+  #   # events at the start and all the :off events at the end.
+  #   Enum.map(raw, &(Enum.at(&1, 0))) ++ Enum.map(raw, &(Enum.at(&1, 1)))
+  # end
 
-  @spec new(event_type(), Chord.t(), integer()) :: [t()]
-  def new(:chord, chord, tpqn) when chord.chord == nil do
-    [first | others] = chord.notes
-    first_event = first_chord_note(first, chord.duration, tpqn)
-    other_events = Enum.map(others, &(other_chord_notes(&1)))
-    raw = [first_event | other_events]
-    # raw is now a list of :on :off pairs, we want to gather all the :on
-    # events at the start and all the :off events at the end.
-    Enum.map(raw, &(Enum.at(&1, 0))) ++ Enum.map(raw, &(Enum.at(&1, 1)))
-  end
+  # @spec new(event_type(), Chord.t(), integer()) :: [t()]
+  # def new(:chord, chord, tpqn) when chord.chord == nil do
+  #   [first | others] = chord.notes
+  #   first_event = first_chord_note(first, chord.duration, tpqn)
+  #   other_events = Enum.map(others, &(other_chord_notes(&1)))
+  #   raw = [first_event | other_events]
+  #   # raw is now a list of :on :off pairs, we want to gather all the :on
+  #   # events at the start and all the :off events at the end.
+  #   Enum.map(raw, &(Enum.at(&1, 0))) ++ Enum.map(raw, &(Enum.at(&1, 1)))
+  # end
 
   @spec new(event_type(), Chord.t(), integer()) :: [t()]
   def new(:chord, chord, tpqn) do
